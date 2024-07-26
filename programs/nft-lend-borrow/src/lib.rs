202bfcb02a220c892686226a7de851798a27c85b
huyenkimiko <FarleyWaltner349@gmail.com>
2024-07-26 11:23:36 +0700

    Update lib.rs

--- programs/nft-lend-borrow/src/lib.rs
+++ programs/nft-lend-borrow/src/lib.rs
@@ -6,7 +6,7 @@ pub use errors::ErrorCodes;
 pub use instructions::*;
 pub use states::*;
 
-declare_id!("<PLACE YOUR ADDRESS HERE>");
+declare_id!("66vpm2SwKfUAXwS2J5onJbKZCYLqV9x4ZEAxTrnufKFH");
 
 #[program]
 pub mod nft_lend_borrow {
